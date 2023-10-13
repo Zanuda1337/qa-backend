62fc7539f1b46d12eed40c480dce8048590d6878
Zanuda1337 <dsnake456@gmail.com>
2023-10-13 05:28:09 +0300

    add images route & static support, fix error handler

--- middleware/errorHandlingMiddleware.js
+++ middleware/errorHandlingMiddleware.js
@@ -1,1 +1,1 @@
-import ApiError from "../error/apiError.js";export const errorHandler = (err, req, res) => {	if(err instanceof ApiError) {		return res.status(err.status).json({message: err.message})	}	return res.status(500).json({message: 'Unexpected error'});}
+import ApiError from "../error/apiError.js";export const errorHandler = (err, req, res, next) => {	if(err instanceof ApiError) {		return res.status(err.status).json({message: err.message})	}	return res.status(500).json({message: 'Unexpected error'});}
